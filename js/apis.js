9b7a3afa90fb31b3c63d3690e06f706f370c6fdc
lindolo25 <lindolo25@outlook.com>
2018-10-13 21:16:57 -0400

    completed most of the functionallity for google maps API.

--- js/apis.js
+++ js/apis.js
@@ -3,9 +3,9 @@ console.log("it all starts here.");
 var apis = 
 {
     resultsMax: 10,
+    currentMap: null,
     eventfulKey : "NpnNdQRSnct6h9mZ",
     everbriteKey: "2ZICJIQQLA7K6R7F664Y",
-    googleKey: "AIzaSyClc949NkCbUZ5VS2OB7Q1bamI6fBCfa3o",
 
     eventfulVenues: function(keyword, location, callback)
     {
@@ -19,7 +19,7 @@ var apis =
         };
         EVDB.API.call("/venues/search", oArgs, function(data) 
         { 
-            apis.callbacks.eventfulVenues(data, callback); 
+            apis.listeners.eventful.eventfulVenues(data, callback); 
         }); 
     },
 
@@ -34,29 +34,138 @@ var apis =
         }).then(callback);
     },
 
-    getMap: function(lat, lng)
+    maps: 
     {
-        var mapObject = $('<div id="map">');
-        console.log(mapObject);
-        map = new google.maps.Map(mapObject[0], {
-            center: {lat: lat, lng: lng},
-            zoom: 8
-          });
-        return mapObject;
-    },
+        getNewMap: function(lat, lng)
+        {
+            var mapObject = $('<div class="map">');
+            console.log(mapObject);
+            map = new google.maps.Map(mapObject[0],
+            {
+                center: {lat: lat, lng: lng},
+                zoom: 8
+            });
+            
+            return { 
+                map: map, 
+                element: mapObject, 
+                markers: [],  
+                infoWindows: [],
 
-    callbacks:
+                bindAutocomplete: function(input) { apis.maps.bindAutocomplete(input, this); },
+                addMarker: function (lng, lat, title, desc) { apis.maps.addMarker(lng, lat, title, desc, this); },
+                clearMarkers: function() { apis.maps.clearMarkers(this); }
+            };
+        },
+
+        bindAutocomplete(input, map)
+        {
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+            autocomplete.bindTo('bounds', map.map);
+            autocomplete.addListener('place_changed',function() 
+            {
+                apis.maps.listeners.autocompletePlaceChanged(autocomplete, map);
+            });
+        },
+
+        addMarker: function(lng, lat, title, desc, map) 
+        {
+            var loc = new google.maps.LatLng(lat, lng);
+            var marker = new google.maps.Marker({ map: map.map, anchorPoint: new google.maps.Point(0, -29) });            
+            marker.setVisible(false);
+            marker.setPosition(loc);
+            marker.setVisible(true);
+
+            map.map.setCenter(loc);
+            map.map.setZoom(17);
+
+            infoWindow = apis.maps.addInfoWindows(title, desc);
+            marker.addListener('click', function() { infoWindow.open(map, marker); });
+            
+            map.markers.push(marker);
+            return(marker);
+        },
+
+        addInfoWindows: function(title, desc)
+        {
+            var infowindow = new google.maps.InfoWindow();
+            var infowindowContent = $('<div class="info-window"><h3 class="title">'+ title +'</h3><p>'+ desc +'</p></div>')[0];
+            infowindow.setContent(infowindowContent);
+            infowindow.close();
+            
+            return infowindow;
+        },
+
+        clearMarkers: function(map) 
+        {
+            this.setMapOnAll(null, map);
+            map.markers = [];
+        },
+
+        setMapOnAll: function (value, map) 
+        {
+            for (var i = 0; i < map.markers.length; i++) 
+            {
+                map.markers[i].setMap(value);
+            }
+        },
+        
+        listeners:
+        {
+            autocompletePlaceChanged(autocomplete, map)
+            {
+                var place = autocomplete.getPlace();
+                console.log(place);
+
+                if (!place.geometry) 
+                {
+                    window.alert("No details available for input: '" + place.name + "'");
+                    return;
+                }
+                
+                if (place.geometry.viewport) 
+                {
+                    map.map.fitBounds(place.geometry.viewport);
+                }
+
+                var address = '';
+                if (place.address_components)
+                {
+                    address = [
+                        (place.address_components[0] && place.address_components[0].short_name || ''),
+                        (place.address_components[1] && place.address_components[1].short_name || ''),
+                        (place.address_components[2] && place.address_components[2].short_name || '')
+                    ]
+                    .join(', ');
+                }
+
+                apis.maps.addMarker(place.geometry.location.lng(), place.geometry.location.lat(), place.name, address, map);
+            }
+        }
+    },
+    
+    listeners:
     {
-        eventfulVenues: function(data, callback) { callback(data); }
+        eventful: 
+        {
+            eventfulVenues: function(data, callback) { callback(data); }
+        }
     }
 }
-
-function initMap() {}
 
 $(document).ready(function()
 {
     apis.eventfulVenues("Restaurants", "Miami", function(response) { console.log(response); });
     apis.everbriteSearchEvents("bike riding", "Miami", function(response) { console.log(response) });
-    $('body').append(apis.getMap(-34.397, 150.644));
+
+
+    currentMap = apis.maps.getNewMap(-34.397, 150.644);
+    $('body').append(currentMap.element);
+
+    currentMap.bindAutocomplete($("#cop-test")[0]);
+    
+
+
 });
 
